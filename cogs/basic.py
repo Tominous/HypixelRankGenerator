50e441f1950f083fef767349ef268b93a7352b1c
ConorTheDev <conorbyrne06@icloud.com>
2019-07-10 10:39:54 +0100

    ✨ Add ping command
    This commit adds the ping command which can be useful to see the bot and API ping

--- cogs/basic.py
+++ cogs/basic.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 
 import json
-
+import datetime
+import time
 
 class Basic(commands.Cog):
 
@@ -14,11 +15,11 @@ class Basic(commands.Cog):
         self.bot = bot
         self.prefix = config['prefix']
 
-    # Define a new command
+    # Define the help command
     @commands.command(
         name='help',
         description='Displays a help menu',
-        aliases=['p']
+        aliases=['h']
     )
     async def help_command(self, ctx):
         embed = discord.Embed(colour=discord.Color.gold(
@@ -31,6 +32,36 @@ class Basic(commands.Cog):
             ctx.author.avatar_url))  # Sets the footer of the embed
 
         await ctx.send(embed=embed)
+        return
+
+    # Define the ping command
+    @commands.command(
+        name='ping',
+        description='Calculates the bot ping (latency)',
+        aliases=['p']
+    )
+    async def ping_command(self, ctx):
+        heartbeat = round(self.bot.latency * 1000)  # Get ping to Discord's Gateway
+
+        a = round(time.time() * 1000)
+        message: discord.Message = await ctx.send(':ping_pong: **Pinging...**')
+        b = round(time.time() * 1000)
+        # Gets the amount of time it takes to send a message
+        elapsed = round(b - a)
+
+        embed = discord.Embed(colour=discord.Color.gold(
+        ), title=f":ping_pong: Pong!")
+
+        embed.add_field(name="Ping",
+                        value=f"{elapsed}ms")
+
+        embed.add_field(name="API Heartbeat",
+                        value=f"{heartbeat}ms")
+
+        embed.set_footer(text=f"Requested by: {ctx.author.name}#{ctx.author.discriminator}", icon_url=str(
+            ctx.author.avatar_url))  # Sets the footer of the embed
+
+        await message.edit(content="", embed=embed)
         return
 
 
